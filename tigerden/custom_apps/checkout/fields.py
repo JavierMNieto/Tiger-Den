8a7be310c60e47ff446fa7258b72a84fb262bb93
Javier <javiermnieto89@gmail.com>
2020-02-05 20:04:44 -0600

    added ListTextWidget

---
+++ tigerden/custom_apps/checkout/fields.py
@@ -0,0 +1,21 @@
+from django import forms
+
+class ListTextWidget(forms.TextInput):
+    """
+    Textfield with options in datalist
+    See usage at https://stackoverflow.com/a/32791625/9499790
+    """
+    def __init__(self, data_list, name, *args, **kwargs):
+        super(ListTextWidget, self).__init__(*args, **kwargs)
+        self._name = name
+        self._list = data_list
+        self.attrs.update({'list': 'list__{}'.format(self._name)})
+
+    def render(self, name, value, attrs=None):
+        text_html = super(ListTextWidget, self).render(name, value, attrs=attrs)
+        data_list = '<datalist id="list__{}">'.format(self._name)
+        for item in self._list:
+            data_list += '<option value="{}">{}</option>'.format(item[0], item[1])
+        data_list += '</datalist>'
+
+        return text_html + data_list
