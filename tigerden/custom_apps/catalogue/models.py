1a81f754899124e8204e473ad055061677e00213
Javier <javiermnieto89@gmail.com>
2020-02-05 20:03:25 -0600

    disabled stock tracking and shipping

--- tigerden/custom_apps/catalogue/models.py
+++ tigerden/custom_apps/catalogue/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 from oscar.apps.catalogue.abstract_models import AbstractProduct, AbstractProductClass
 
-class AbstractProductClass(AbstractProductClass):
-    requires_shipping = False
-
-    track_stock = False
+class ProductClass(AbstractProductClass):
+    # Not implementing stock tracking
+    track_stock = models.BooleanField(_("Track stock levels?"), default=False)
+    
+    # Not implementing shipping
+    requires_shipping = models.BooleanField(_("Requires shipping?"), default=False)
 
 from oscar.apps.catalogue.models import *
