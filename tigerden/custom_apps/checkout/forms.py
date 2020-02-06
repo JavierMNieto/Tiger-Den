a226bbb91d2933b74480dcd84b4e4816a2d8cca8
Javier <javiermnieto89@gmail.com>
2020-02-05 20:05:48 -0600

    Added supervisor group

--- tigerden/custom_apps/checkout/forms.py
+++ tigerden/custom_apps/checkout/forms.py
@@ -1,8 +1,28 @@
-from oscar.apps.checkout.forms import ShippingAddressForm
+#from oscar.apps.checkout.forms import 
+from django import forms
+from django.contrib import auth
 
-class ShippingAddressForm(ShippingAddressForm):
+from . import fields
 
-    class Meta:
-        fields = [
-            'first_name', 'last_name', 'supervisor'
-        ]
+def get_supervisors_tuple():
+    supervisors = ()
+    
+    for user in auth.models.User.objects.filter(groups__name='Supervisor'):
+        name = user.get_full_name()
+        if name.strip() == "":
+            name = user.email
+        supervisors += ((name, user.pk),)
+    
+    return supervisors
+
+class GuestSupervisorForm(forms.Form):
+    """
+    Form to get guest's supervisor
+    """
+    
+    supervisors = forms.CharField(required=True)
+    
+    def __init__(self, *args, **kwargs):
+        super(GuestSupervisorForm, self).__init__(*args, **kwargs)
+        
+        self.fields['supervisors'].widget = fields.ListTextWidget(data_list=get_supervisors_tuple(), name='supervisor-list')
