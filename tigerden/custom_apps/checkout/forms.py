69d7e06101d45af83acdbcc4a85a29ba47d736f9
Javier <javiermnieto89@gmail.com>
2020-02-08 23:42:03 -0600

    Added supervisor/student checkouts

--- tigerden/custom_apps/checkout/forms.py
+++ tigerden/custom_apps/checkout/forms.py
@@ -1,13 +1,13 @@
 #from oscar.apps.checkout.forms import 
 from django import forms
-from django.contrib import auth
+from django.contrib.auth.models import User
 
 from . import fields
 
 def get_supervisors_tuple():
     supervisors = ()
     
-    for user in auth.models.User.objects.filter(groups__name='Supervisor'):
+    for user in User.objects.filter(groups__name='Supervisor'):
         name = user.get_full_name()
         if name.strip() == "":
             name = user.email
@@ -15,16 +15,36 @@ def get_supervisors_tuple():
     
     return supervisors
 
-class GuestSupervisorForm(forms.Form):
+class SupervisorForm(forms.Form):
     """
-    Form to get guest's supervisor
+    Form to get user's supervisor
     """
     
     supervisor = forms.CharField(required=True)
     
     def __init__(self, *args, **kwargs):
-        super(GuestSupervisorForm, self).__init__(*args, **kwargs)
+        super(SupervisorForm, self).__init__(*args, **kwargs)
         
         self.fields['supervisor'].widget = fields.ListTextWidget(data_list=get_supervisors_tuple(), 
                                                                  name='supervisor-list', 
-                                                                 attrs={'class': 'form-control m-1'})
+                                                                 attrs={'class': 'form-control'})
+
+class LocationForm(forms.Form):
+    """
+    """
+    
+    location = forms.CharField(required=True, max_length=50) # change css
+    
+    def __init__(self, *args, **kwargs):
+        super(LocationForm, self).__init__(*args, **kwargs)
+
+class PaymentMethodForm(forms.Form):
+    """
+    
+    """
+    payment_method = forms.ChoiceField() # change css
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['payment_method'].choices = ((0, "Cash"), (1, "Balance"))
