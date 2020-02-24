6fdbcbe3102214d74903cf8f20e8dd475b1a5a13
Javier <javiermnieto89@gmail.com>
2020-02-24 17:58:52 -0600

    Changed product stock to only display availability

--- tigerden/custom_apps/partner/availability.py
+++ tigerden/custom_apps/partner/availability.py
@@ -1,9 +1,28 @@
 from oscar.apps.partner import availability
+from django.utils.translation import gettext_lazy as _
 
-class Base(availability.Base):
-    extra_message = ''
-    
+class Available(availability.Available):
+    code = 'instock'
+
 class Unavailable(availability.Unavailable):
-    def __init__(self, extra_message=''):
+    code = 'outofstock'
+    
+    def __init__(self, message=_("Unavailable")):
         super().__init__()
-        self.extra_message = extra_message
+        self.message = message
+        
+class StockRequired(availability.StockRequired):
+    @property
+    def short_message(self):
+        if self.num_available > 0:
+            return _("Available")
+        return _("Unavailable")
+    
+    @property
+    def message(self):
+        """
+        Full availability text, suitable for detail pages.
+        """
+        if self.num_available > 0:
+            return _("Available")
+        return _("Unavailable")
