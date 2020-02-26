1ac2b00af0dc63a8f6116687ea435c44119c7d3b
Javier <javiermnieto89@gmail.com>
2020-02-26 17:51:03 -0600

    Fixed empty options showing in cart

--- tigerden/custom_apps/basket/models.py
+++ tigerden/custom_apps/basket/models.py
@@ -1,1 +1,16 @@
+from oscar.apps.basket.abstract_models import AbstractLine
+from django.utils.encoding import smart_str
+
+class Line(AbstractLine):
+    @property
+    def description(self):
+        d = smart_str(self.product)
+        ops = []
+        for attribute in self.attributes.all():
+            if attribute.value.strip() != "":
+                ops.append("%s = '%s'" % (attribute.option.name, attribute.value))
+        if ops:
+            d = "%s (%s)" % (d, ", ".join(ops))
+        return d
+
 from oscar.apps.basket.models import *  # noqa isort:skip
