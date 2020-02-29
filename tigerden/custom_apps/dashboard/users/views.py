c90c67f476b49527a061dfededd926487d197638
Javier <javiermnieto89@gmail.com>
2020-02-28 22:22:39 -0600

    fixed change supervisor button

--- tigerden/custom_apps/dashboard/users/views.py
+++ tigerden/custom_apps/dashboard/users/views.py
@@ -3,20 +3,23 @@ from django.http import Http404, HttpResponseRedirect
 from django.utils.translation import gettext_lazy as _
 from django.contrib import messages
 from django.urls import reverse
+from oscar.core.compat import get_user_model
 
 from django.contrib.auth.models import Group
+
+User = get_user_model()
 
 supervisor_group = Group.objects.get(name='Supervisor')
 
 class UserDetailView(views.UserDetailView):
     def post(self, request, *args, **kwargs):
         if "user_action" in request.POST:
-            return self.change_supervisor(request)
+            return self.change_supervisor(request, kwargs['pk'])
 
         return self.reload_page(error=_("No valid action submitted"))
 
-    def change_supervisor(self, request):
-        user = request.user
+    def change_supervisor(self, request, pk):
+        user = User.objects.all().get(pk=pk)
 
         if user.is_supervisor():
             supervisor_group.user_set.remove(user)
@@ -25,11 +28,11 @@ class UserDetailView(views.UserDetailView):
             supervisor_group.user_set.add(user)
             messages.info(request, "Added supervisor permissions to " + user.label())
         
-        return self.reload_page()
+        return self.reload_page(user.pk)
     
-    def reload_page(self, fragment=None, error=None):
+    def reload_page(self, pk, fragment=None, error=None):
         url = reverse('dashboard:user-detail',
-                      kwargs={'pk': self.request.user.id})
+                      kwargs={'pk': pk})
         if fragment:
             url += '#' + fragment
         if error:
