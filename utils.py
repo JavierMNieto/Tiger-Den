675c383bf71ac53a12d3c975161e338a5356ba36
Javier Nieto <javiermnieto89@gmail.com>
2023-01-09 06:49:42 +0100

    init utils script

---
+++ utils.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+
+SRC_DIR = "tigerden"
+MANAGE = os.path.join(SRC_DIR, "manage.py")
+FIXTURES_DIR = os.path.join(SRC_DIR, "fixtures")
+FIXTURES = [
+    "attribute_option_groups.json",
+    "attribute_options.json",
+    "options.json",
+    "product_classes.json",
+    "products.json",
+    "categories.json",
+    "product_categories.json",
+    "product_images.json",
+    "stock_records.json",
+    "account_types.json",
+    "accounts.json"
+]
+
+# windows activate virtualenv
+windows_activate = os.path.abspath("venv/Scripts/activate.bat")
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help="commands")
+
+    # setup virtualenv
+    env_parser = subparsers.add_parser("setup", help="setup environment")
+    env_parser.set_defaults(func=setup_env)
+
+    # initialize db
+    db_parser = subparsers.add_parser("initdb", help="initialize database")
+    db_parser.set_defaults(func=initialize_db)
+
+    # runserver
+    run_parser = subparsers.add_parser("runserver", help="runserver")
+    run_parser.set_defaults(func=runserver)
+
+    return parser.parse_args()
+
+
+def runserver(args):
+    os.system(f"python {MANAGE} runserver")
+
+
+def setup_env(args):
+    pass
+
+
+# def activate_env(args):
+#     os.system(f'"{windows_activate}"')
+
+
+def initialize_db(args):
+    # os.system(f"python {MANAGE} migrate --run-syncdb")
+    # os.system(f"python {MANAGE} createsuperuser")
+    for fixture in FIXTURES:
+        fixture_path = os.path.join(FIXTURES_DIR, fixture)
+        os.system(f"python {MANAGE} loaddata {fixture_path}")
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    args.func(args)
