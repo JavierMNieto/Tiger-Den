0741a3ccce86a5bedc17c23c4c7993e5664ed034
Javier Nieto <javiermnieto89@gmail.com>
2023-01-19 20:57:40 -0600

    Revert "fix media path"
    This reverts commit 1ac46c1d9accff8ae1d9b324791495e03abbd2cb.

--- tigerden/tigerden/settings.py
+++ tigerden/tigerden/settings.py
@@ -197,7 +197,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = location('public/media')
-MEDIA_ROOT = location('public/media')
+MEDIA_ROOT = location('tigerden/public/media')
 STATIC_ROOT = location('public/static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
